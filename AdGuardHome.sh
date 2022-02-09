af14b6c8c832deed23c207f21b587fe0cd27f3e1
jumpsmm7 <49514613+jumpsmm7@users.noreply.github.com>
2022-02-09 16:45:29 -0500

    Update AdGuardHome.sh

--- AdGuardHome.sh
+++ AdGuardHome.sh
@@ -41,17 +41,7 @@ stop_AdGuardHome () {
   . /opt/etc/init.d/rc.func
   if [ -f "/tmp/stats.db" ]; then rm -rf "/tmp/stats.db" >/dev/null 2>&1; fi
   if [ -f "/tmp/sessions.db" ]; then rm -rf "/tmp/sessions.db" >/dev/null 2>&1; fi
-  local MAN_PID 
-  MAN_PID="$(pidof S99AdGuardHome)"
-  local PID
-  if [ "$MAN_PID" ]; then
-    for PID in $MAN_PID; do
-      if [ "$(awk '{ print }' "/proc/${PID}/cmdline" | grep AdGuardHome)" ]; then
-        kill -9 "$PID"
-        break
-      fi
-    done
-  fi
+  $SCRIPT_LOC stop
   service restart_dnsmasq >/dev/null 2>&1
 }
 
