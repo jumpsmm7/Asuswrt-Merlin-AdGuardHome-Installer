fdebc01f2b832dca8fca3a83bf4b90009e2e3763
jumpsmm7 <49514613+jumpsmm7@users.noreply.github.com>
2022-02-08 23:41:22 -0500

    Update AdGuardHome.sh

--- AdGuardHome.sh
+++ AdGuardHome.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-if [ -f "/opt/etc/init.d/S99AdGuardHome" ]; then . /opt/etc/init.d/S99AdGuardHome; else exit 1; fi
+if [ ! -f "/opt/etc/init.d/S99AdGuardHome" ]; then exit 1; fi
 
+ARGS="$@"
 NAME="$(basename $0)[$$]"
 
 dnsmasq_params () {
-  if [ "$(pidof AdGuardHome)" ] && [ "$(nvram get ipv6_fw_enable)" != "1" ]; then printf "%s\n" "port=553" "local=/$(nvram get lan_ipaddr | awk 'BEGIN{FS="."}{print $2"."$1".in-addr.arpa"}')/" "local=/10.in-addr.arpa/" "dhcp-option=lan,6,0.0.0.0" >> $1; fi
-  if [ "$(pidof AdGuardHome)" ] && [ "$(nvram get ipv6_fw_enable)" = "1" ]; then printf "%s\n" "port=553" "local=/$(nvram get lan_ipaddr | awk 'BEGIN{FS="."}{print $2"."$1".in-addr.arpa"}')/" "local=/10.in-addr.arpa/" "local=/$(nvram get ipv6_prefix | sed "s/://g;s/^.*$/\n&\n/;tx;:x;s/\(\n.\)\(.*\)\(.\n\)/\3\2\1/;tx;s/\n//g;s/\(.\)/\1./g;s/$/ip6.arpa/")/" "dhcp-option=lan,6,0.0.0.0" >> $1; fi
+  local CONFIG
+  CONFIG="/etc/dnsmasq.conf"
+  if [ "$(pidof AdGuardHome)" ] && [ "$(nvram get ipv6_fw_enable)" != "1" ]; then printf "%s\n" "port=553" "local=/$(nvram get lan_ipaddr | awk 'BEGIN{FS="."}{print $2"."$1".in-addr.arpa"}')/" "local=/10.in-addr.arpa/" "dhcp-option=lan,6,0.0.0.0" >> $CONFIG; fi
+  if [ "$(pidof AdGuardHome)" ] && [ "$(nvram get ipv6_fw_enable)" = "1" ]; then printf "%s\n" "port=553" "local=/$(nvram get lan_ipaddr | awk 'BEGIN{FS="."}{print $2"."$1".in-addr.arpa"}')/" "local=/10.in-addr.arpa/" "local=/$(nvram get ipv6_prefix | sed "s/://g;s/^.*$/\n&\n/;tx;:x;s/\(\n.\)\(.*\)\(.\n\)/\3\2\1/;tx;s/\n//g;s/\(.\)/\1./g;s/$/ip6.arpa/")/" "dhcp-option=lan,6,0.0.0.0" >> $CONFIG; fi
   if [ -n "$(route | grep "br" | grep -v "br0" | grep -E "^(192\.168|10\.|172\.1[6789]\.|172\.2[0-9]\.|172\.3[01]\.)" | awk '{print $1}' | sed -e 's/[0-9]$/1/' | sed -e ':a; N; $!ba;s/\n/ /g')" ]; then
     iCOUNT="1"
     for iVARS in $(route | grep "br" | grep -v "br0" | grep -E "(192\.168|10\.|172\.1[6789]\.|172\.2[0-9]\.|172\.3[01]\.)" | awk '{print $8}' | sed -e ':a; N; $!ba;s/\n/ /g'); do
@@ -23,7 +26,7 @@ dnsmasq_params () {
     for i in $COUNT; do
       NIVARS="$(printf "%s\n" "$IVARS" | cut -d' ' -f$i)"
       NDVARS="$(printf "%s\n" "$DVARS" | cut -d' ' -f$i)"
-      if [ "$(pidof AdGuardHome)" ]; then printf "%s\n" "dhcp-option=$NIVARS,6,$NDVARS" >> $1; fi
+      if [ "$(pidof AdGuardHome)" ]; then printf "%s\n" "dhcp-option=$NIVARS,6,$NDVARS" >> $CONFIG; fi
     done
   fi
 }
@@ -31,7 +34,7 @@ dnsmasq_params () {
 start_AdGuardHome () {
   killall -q AdGuardHome
   logger -st "$NAME" "Starting AdGuardHome"
-  $PREARGS AdGuardHome $ARGS >/dev/null 2>&1 </dev/null &
+  nohup env TZ=/etc/localtime AdGuardHome $ARGS >/dev/null 2>&1 </dev/null &
   if [ ! -f "/tmp/stats.db" ]; then ln -sf "${WORK_DIR}/data/stats.db" "/tmp/stats.db" >/dev/null 2>&1; fi
   if [ ! -f "/tmp/sessions.db" ]; then ln -sf "${WORK_DIR}/data/sessions.db" "/tmp/sessions.db" >/dev/null 2>&1; fi
 }
