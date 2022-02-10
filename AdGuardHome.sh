0e430b0f562b1713419fdc65551ec78cc49f6ed5
jumpsmm7 <49514613+jumpsmm7@users.noreply.github.com>
2022-02-09 21:46:10 -0500

    Update AdGuardHome.sh

--- AdGuardHome.sh
+++ AdGuardHome.sh
@@ -2,11 +2,11 @@
 
 [ ! -f "/opt/etc/init.d/S99AdGuardHome" ] && exit 1
 
-NAME="$(basename $0)[$$]"
+NAME="$(basename "$0")[$$]"
 SCRIPT_LOC="/opt/etc/init.d/S99AdGuardHome"
 
 dnsmasq_params () {
-  local CONFIG
+  CONFIG=""
   CONFIG="/etc/dnsmasq.conf"
   if [ "$(pidof AdGuardHome)" ] && [ -z "$(nvram get ipv6_rtr_addr)" ]; then printf "%s\n" "port=553" "local=/$(nvram get lan_ipaddr | awk 'BEGIN{FS="."}{print $2"."$1".in-addr.arpa"}')/" "local=/10.in-addr.arpa/" "dhcp-option=lan,6,0.0.0.0" >> $CONFIG; fi
   if [ "$(pidof AdGuardHome)" ] && [ -n "$(nvram get ipv6_rtr_addr)" ]; then printf "%s\n" "port=553" "local=/$(nvram get lan_ipaddr | awk 'BEGIN{FS="."}{print $2"."$1".in-addr.arpa"}')/" "local=/10.in-addr.arpa/" "local=/$(nvram get ipv6_prefix | sed "s/://g;s/^.*$/\n&\n/;tx;:x;s/\(\n.\)\(.*\)\(.\n\)/\3\2\1/;tx;s/\n//g;s/\(.\)/\1./g;s/$/ip6.arpa/")/" "dhcp-option=lan,6,0.0.0.0" >> $CONFIG; fi
@@ -24,8 +24,8 @@ dnsmasq_params () {
       dCOUNT="$((dCOUNT+1))"
     done
     for i in $COUNT; do
-      NIVARS="$(printf "%s\n" "$IVARS" | cut -d' ' -f$i)"
-      NDVARS="$(printf "%s\n" "$DVARS" | cut -d' ' -f$i)"
+      NIVARS="$(printf "%s\n" "$IVARS" | cut -d' ' -f"$i")"
+      NDVARS="$(printf "%s\n" "$DVARS" | cut -d' ' -f"$i")"
       if [ "$(pidof AdGuardHome)" ]; then printf "%s\n" "dhcp-option=$NIVARS,6,$NDVARS" >> $CONFIG; fi
     done
   fi
@@ -46,9 +46,10 @@ stop_AdGuardHome () {
 start_monitor () {
   trap "" 1
   while [ "$(nvram get ntp_ready)" -eq 0 ]; do sleep 1; done
-  local NW_STATE
-  local RES_STATE
-  local COUNT=0
+  NW_STATE=""
+  RES_STATE=""
+  COUNT=""
+  COUNT="0"
   while true; do  
     if [ "$COUNT" -eq 90 ]; then
       COUNT=0
@@ -61,7 +62,7 @@ start_monitor () {
       if [ -z "$(pidof AdGuardHome)" ]; then
         logger -st "$NAME" "Warning: AdGuardHome is dead"
         start_AdGuardHome
-      elif [ "$NW_STATE" = "0" -a "$RES_STATE" != "0" ]; then
+      elif { [ "$NW_STATE" = "0" ] && [ "$RES_STATE" != "0" ]; }; then
         logger -st "$NAME" "Warning: AdGuardHome is not responding"
         start_AdGuardHome
       fi
@@ -71,21 +72,21 @@ start_monitor () {
 }
 
 timezone () {
-  local SANITY
-  local NOW
-  local TIMEZONE
-  local TARGET
-  local LINK
-  SANITY="$(date -u -r $0 '+%s')"
+  SANITY=""
+  NOW=""
+  TIMEZONE=""
+  TARGET=""
+  #LINK=""
+  SANITY="$(date -u -r "$0" '+%s')"
   NOW="$(date -u '+%s')"
   TIMEZONE="/opt/etc/AdGuardHome/localtime"
   TARGET="/etc/localtime"
-  LINK="$(readlink $TARGET)"
+  #LINK="$(readlink $TARGET)"
   if [ -f "$TARGET" ]; then
       if [ "$NOW" -ge "$SANITY" ]; then
         touch "$0"
       elif [ "$NOW" -le "$SANITY" ]; then
-        date -u -s "$(date -u -r \"$0\" '+%Y-%m-%d %H:%M:%S')"
+        date -u -s "$(date -u -r \""$0"\" '+%Y-%m-%d %H:%M:%S')"
       fi 
   elif [ -f "$TIMEZONE" ] || [ ! -f "$TARGET" ]; then
     ln -sf $TIMEZONE $TARGET
