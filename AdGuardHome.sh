2d10a3fc5d494fe67704e2099a640c2bc5eac9c7
jumpsmm7 <49514613+jumpsmm7@users.noreply.github.com>
2022-02-12 19:55:14 -0500

    Update AdGuardHome.sh

--- AdGuardHome.sh
+++ AdGuardHome.sh
@@ -5,7 +5,7 @@ SCRIPT_LOC="$(readlink -f "$0")"
 UPPER_SCRIPT="/opt/etc/init.d/S99AdGuardHome"
 LOWER_SCRIPT="/opt/etc/init.d/rc.func.AdGuardHome"
 
-[ ! -f "$UPPER_SCRIPT" ] && exit 1 || UPPER_SCRIPT_LOC="source $UPPER_SCRIPT"
+[ ! -f "$UPPER_SCRIPT" ] && exit 1 || UPPER_SCRIPT_LOC=". $UPPER_SCRIPT"
 [ ! -f "$LOWER_SCRIPT" ] && exit 1 || LOWER_SCRIPT_LOC=". $LOWER_SCRIPT"
 [ -z "$PROCS" ] && $UPPER_SCRIPT_LOC
 
