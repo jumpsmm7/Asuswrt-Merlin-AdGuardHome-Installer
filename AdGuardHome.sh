12d7b0004dcaf66a35bc12b51541029116d6b569
jumpsmm7 <49514613+jumpsmm7@users.noreply.github.com>
2022-02-16 00:20:49 -0500

    Update AdGuardHome.sh

--- AdGuardHome.sh
+++ AdGuardHome.sh
@@ -55,7 +55,7 @@ script_loc () {
   local LOWER_SCRIPT_LOC
   [ ! -f "$UPPER_SCRIPT" ] && return 1 || UPPER_SCRIPT_LOC=". $UPPER_SCRIPT"
   [ ! -f "$LOWER_SCRIPT" ] && return 1 || LOWER_SCRIPT_LOC=". $LOWER_SCRIPT"
-  [ -z "$PROCS" ] && $UPPER_SCRIPT_LOC
+  [ "$SCRIPT_LOC" = "$(readlink -f "$0")" ] && $UPPER_SCRIPT_LOC
 }
 
 start_AdGuardHome () {
