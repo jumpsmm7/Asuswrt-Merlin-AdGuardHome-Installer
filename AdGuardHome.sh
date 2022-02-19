cd2c18dfb906f0677ee4533e073d983fa2bd13db
jumpsmm7 <49514613+jumpsmm7@users.noreply.github.com>
2022-02-19 18:20:14 -0500

    Update AdGuardHome.sh
    * one more fix

--- AdGuardHome.sh
+++ AdGuardHome.sh
@@ -6,7 +6,7 @@ UPPER_SCRIPT="/opt/etc/init.d/S99AdGuardHome"
 LOWER_SCRIPT="/opt/etc/init.d/rc.func.AdGuardHome"
 if [ -f "$UPPER_SCRIPT" ]; then UPPER_SCRIPT_LOC=". $UPPER_SCRIPT"; fi
 if [ -f "$LOWER_SCRIPT" ]; then LOWER_SCRIPT_LOC=". $LOWER_SCRIPT"; fi
-if [ "$1" = "init-start" ]; then while [ ! -f "$UPPER_SCRIPT" ]; do sleep 1; done ; fi
+if [ "$1" = "init-start" ] && [ ! -f "$UPPER_SCRIPT" ]; then timezone; while [ ! -f "$UPPER_SCRIPT" ]; do sleep 1; done; fi
 if [ "$UPPER_SCRIPT" != "$SCRIPT_LOC" ] && [ -f "$UPPER_SCRIPT" ]; then exec $UPPER_SCRIPT $@ && exit; fi
 NAME="$(basename "$0")[$$]"
 
