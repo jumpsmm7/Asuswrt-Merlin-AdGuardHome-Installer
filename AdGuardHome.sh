87ecb31566e0e9a71e01e62b3322d5a7807464d6
jumpsmm7 <49514613+jumpsmm7@users.noreply.github.com>
2022-02-28 22:04:06 -0500

    Update AdGuardHome.sh

--- AdGuardHome.sh
+++ AdGuardHome.sh
@@ -156,8 +156,8 @@ case "$1" in
     if [ "$1" = "init-start" ]; then { printf "1" > /proc/sys/vm/overcommit_memory; }; start_AdGuardHome; { "$SCRIPT_LOC" monitor-start >/dev/null 2>&1; }; fi
     if [ "$1" = "services-stop" ]; then stop_AdGuardHome; { [ -n "$(pidof "$PROCS" "S99${PROCS}" "${PROCS}.sh")" ] && kill -9 "$(pidof "$PROCS" "S99${PROCS}" "${PROCS}.sh")" 2>/dev/null; }; { [ -n "$(pidof "$PROCS" "S99${PROCS}" "${PROCS}.sh")" ] && killall -q -9 "$PROCS" "S99${PROCS}" "${PROCS}.sh" 2>/dev/null; }; fi
     ;;
-  "check"|*)
-    { lower_script "$1"; } && exit
+  *)
+    { $LOWER_SCRIPT_LOC "$1"; } && exit
     ;;
 esac
 check_dns_environment
