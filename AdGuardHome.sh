669aa8dcd8f9f58de29acb0a00f91b4d0ec07ee9
jumpsmm7 <49514613+jumpsmm7@users.noreply.github.com>
2022-03-02 23:46:38 -0500

    Update AdGuardHome.sh

--- AdGuardHome.sh
+++ AdGuardHome.sh
@@ -142,7 +142,7 @@ unset TZ
 case "$1" in
   "monitor-start")
     trap '{ start_monitor & }' 10
-    { kill -s -10 "$(pidof "S99${PROCS}")" 2>/dev/null || killall -q -10 "S99${PROCS}" 2>/dev/null; }
+    { kill -s 10 "$(pidof "S99${PROCS}")" 2>/dev/null || killall -q -10 "S99${PROCS}" 2>/dev/null; }
     ;;
   "start"|"restart")
     if [ -z "$(pidof "$PROCS")" ]; then { "$SCRIPT_LOC" init-start; }; else start_AdGuardHome; fi
@@ -154,9 +154,10 @@ case "$1" in
     dnsmasq_params
     ;;
   "init-start"|"services-stop")
+    trap '' 10
     timezone
     if [ "$1" = "init-start" ]; then { printf "1" > /proc/sys/vm/overcommit_memory; }; { "$SCRIPT_LOC" monitor-start >/dev/null 2>&1; }; start_AdGuardHome; fi
-    if [ "$1" = "services-stop" ]; then trap 'exit 0' 10; stop_AdGuardHome; { kill -s -10 "$(pidof "S99${PROCS}")" 2>/dev/null || killall -q -10 "S99${PROCS}" 2>/dev/null; }; fi
+    if [ "$1" = "services-stop" ]; then trap 'exit 0' 10; stop_AdGuardHome; { kill -s 10 "$(pidof "S99${PROCS}")" 2>/dev/null || killall -q -10 "S99${PROCS}" 2>/dev/null; }; fi
     ;;
   *)
     { $LOWER_SCRIPT_LOC "$1"; } && exit
