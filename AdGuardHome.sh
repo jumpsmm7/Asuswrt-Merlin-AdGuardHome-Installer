553743cad85cfcaf8f0486507fbd61458bac281e
jumpsmm7 <49514613+jumpsmm7@users.noreply.github.com>
2022-03-03 01:35:46 -0500

    Update AdGuardHome.sh

--- AdGuardHome.sh
+++ AdGuardHome.sh
@@ -144,12 +144,7 @@ timezone () {
 unset TZ
 case "$1" in
   "monitor-start")
-    for PID in $(pidof "S99${PROCS}"); do
-      if { awk '{ print }' "/proc/${PID}/cmdline" | grep -q monitor-start; } && [ "$PID" != "$$" ]; then
-        kill -s 10 "$PID"
-        break
-      fi
-    done
+    for PID in $(pidof "S99${PROCS}"); do if { awk '{ print }' "/proc/${PID}/cmdline" | grep -q monitor-start; } && [ "$PID" != "$$" ]; then { kill -s 10 "$PID" >/dev/null 2>&1; }; break; fi; done
     start_monitor &
     ;;
   "start"|"restart")
@@ -164,7 +159,7 @@ case "$1" in
   "init-start"|"services-stop")
     timezone
     if [ "$1" = "init-start" ]; then { printf "1" > /proc/sys/vm/overcommit_memory; }; { "$SCRIPT_LOC" monitor-start >/dev/null 2>&1; }; start_AdGuardHome; fi
-    if [ "$1" = "services-stop" ]; then trap 'exit 0' 10; stop_AdGuardHome; { kill -s 10 "$(pidof "S99${PROCS}")" 2>/dev/null || killall -q -10 "S99${PROCS}" 2>/dev/null; }; fi
+    if [ "$1" = "services-stop" ]; then stop_AdGuardHome; { for PID in $(pidof "S99${PROCS}"); do if { awk '{ print }' "/proc/${PID}/cmdline" | grep -q monitor-start; } && [ "$PID" != "$$" ]; then { kill -s 10 "$PID" >/dev/null 2>&1; }; break; fi; done; }; fi
     ;;
   *)
     { $LOWER_SCRIPT_LOC "$1"; } && exit
