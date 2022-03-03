bc66835653b71b150a606118caf3b7a49c49d55c
jumpsmm7 <49514613+jumpsmm7@users.noreply.github.com>
2022-03-02 23:07:09 -0500

    Update AdGuardHome.sh

--- AdGuardHome.sh
+++ AdGuardHome.sh
@@ -154,7 +154,8 @@ case "$1" in
     ;;
   "init-start"|"services-stop")
     timezone
-    if [ "$1" = "init-start" ]; then { printf "1" > /proc/sys/vm/overcommit_memory; }; [ -n "$(pidof "S99${PROCS}" | sed "s/\<$$\>//g;s/^[ \t]*//;s/[ \t]*$//")" ] && { kill -s -10 "$(pidof "S99${PROCS}" | sed "s/\<$$\>//g;s/^[ \t]*//;s/[ \t]*$//")"; }; { "$SCRIPT_LOC" monitor-start >/dev/null 2>&1; }; start_AdGuardHome; fi
+    if [ -n "$(pidof "S99${PROCS}" | sed "s/\<$$\>//g")" ]; then { kill -s -10 "$(pidof "S99${PROCS}" | sed "s/\<$$\>//g")"; }; fi
+    if [ "$1" = "init-start" ]; then { printf "1" > /proc/sys/vm/overcommit_memory; }; { "$SCRIPT_LOC" monitor-start >/dev/null 2>&1; }; start_AdGuardHome; fi
     if [ "$1" = "services-stop" ]; then trap 'exit 0' 10; stop_AdGuardHome; { kill -s -10 "$(pidof "S99${PROCS}")" 2>/dev/null || killall -q -10 "S99${PROCS}" 2>/dev/null; }; fi
     ;;
   *)
