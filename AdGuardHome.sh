80de3c4f5b7720c258cb2fb5c589d37f8cb74b3b
jumpsmm7 <49514613+jumpsmm7@users.noreply.github.com>
2022-03-03 21:05:57 -0500

    Update AdGuardHome.sh
    * fix trap clears.

--- AdGuardHome.sh
+++ AdGuardHome.sh
@@ -93,7 +93,7 @@ start_monitor () {
   EXIT="0"
   logger -st "$NAME" "Starting_Monitor: $NAME"
   while true; do
-    if [ "$EXIT" = "1" ]; then logger -st "$NAME" "Stopping_Monitor: $NAME"; trap 1 2 3 10 15; stop_AdGuardHome; break; fi
+    if [ "$EXIT" = "1" ]; then logger -st "$NAME" "Stopping_Monitor: $NAME"; trap - 1 2 3 6 10 15; stop_AdGuardHome; break; fi
     if [ "$COUNT" -gt "90" ]; then COUNT="0"; timezone; fi
     COUNT="$((COUNT + 1))"
     if [ -f "/opt/sbin/AdGuardHome" ]; then
