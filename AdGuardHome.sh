2b4b790e775ea0a8c4c3eb5081086c04d8fdab1a
jumpsmm7 <49514613+jumpsmm7@users.noreply.github.com>
2022-07-02 23:10:27 -0400

    Update AdGuardHome.sh

--- AdGuardHome.sh
+++ AdGuardHome.sh
@@ -276,18 +276,18 @@ case "$1" in
     ;;
   "wan-event")
     case "$(get_wan_setting ifname)" in
+      "")
+        if [ -n "$(pidof AdGuardHome)" ] && [ -n "$MON_PID" ]; then service stop_AdGuardHome >/dev/null 2>&1; fi;
+        ;;
        *)
         case "$3" in
           connected|connecting|init)
             if [ -n "$(pidof AdGuardHome)" ] && [ -n "$MON_PID" ]; then service restart_AdGuardHome >/dev/null 2>&1; elif [ "$(pidof "S99${PROCS}" | wc -w)" -eq "1" ]; then service start_AdGuardHome >/dev/null 2>&1; fi;
             ;;
-          disconnected|stopped|stopping|disabled)         
+          disconnected|stopped|stopping|disabled|*)
             if [ -n "$(pidof AdGuardHome)" ] && [ -n "$MON_PID" ]; then service stop_AdGuardHome >/dev/null 2>&1; fi;
             ;;
         esac;
-        ;;
-      "")
-        if [ -n "$(pidof AdGuardHome)" ] && [ -n "$MON_PID" ]; then service stop_AdGuardHome >/dev/null 2>&1; fi;
         ;;
     esac;
     ;;
