0f21508038dddb74d2549e39c019059b95287dea
jumpsmm7 <49514613+jumpsmm7@users.noreply.github.com>
2026-05-02 19:12:56 -0400

    tools: include AdGuardHome.sh in md5 updates

--- tools/update-md5.sh
+++ tools/update-md5.sh
@@ -6,7 +6,7 @@ set -u
 
 FAILED=0
 UPDATED=0
-TARGETS="installer S99AdGuardHome rc.func.AdGuardHome"
+TARGETS="installer AdGuardHome.sh S99AdGuardHome rc.func.AdGuardHome"
 
 have_cmd() {
 	which "$1" >/dev/null 2>&1
