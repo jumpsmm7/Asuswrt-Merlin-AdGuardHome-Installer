35528602f588b26f7d31b2cafd58d6d31719ba7f
jumpsmm7 <49514613+jumpsmm7@users.noreply.github.com>
2026-06-13 16:45:39 -0400

    fix: abort update when requested backup fails

--- tests/installer-confirmation-failure-propagation.sh
+++ tests/installer-confirmation-failure-propagation.sh
@@ -26,7 +26,9 @@ REMOTE_VER='1'
 REMOTE_BETA='1'
 REMOTE_EDGE='1'
 INFO='Info:'
+ERROR='Error:'
 CHECK_LOG="${ROOT}/continued"
+BACKUP_STATUS=0
 mkdir -p "${BASE_DIR}"
 trap 'rm -rf "${ROOT}"' 0 HUP INT TERM
 
@@ -38,6 +40,12 @@ file_md5() {
 }
 PTXT() {
 	:
+}
+end_op_message() {
+	:
+}
+backup_restore() {
+	return "${BACKUP_STATUS}"
 }
 check_connection() {
 	printf '%s\n' continued >>"${CHECK_LOG}"
@@ -59,4 +67,14 @@ STATUS=$?
 [ "${STATUS}" -eq 2 ] || fail 'backup confirmation failure was not propagated'
 [ ! -e "${CHECK_LOG}" ] || fail 'update continued after backup confirmation failure'
 
-printf '%s\n' 'PASS: installation confirmation failures are propagated'
+read_yesno() {
+	return 0
+}
+BACKUP_STATUS=1
+rm -f "${CHECK_LOG}"
+inst_AdGuardHome update ""
+STATUS=$?
+[ "${STATUS}" -eq 1 ] || fail 'pre-update backup failure was not propagated'
+[ ! -e "${CHECK_LOG}" ] || fail 'update continued after pre-update backup failure'
+
+printf '%s\n' 'PASS: installation confirmation and backup failures are propagated'
