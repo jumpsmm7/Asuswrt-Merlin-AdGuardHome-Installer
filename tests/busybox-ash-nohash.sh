a0497f9cb27090346920139648887b37070cd5b1
SomeWhereOverTheRainbow <49514613+jumpsmm7@users.noreply.github.com>
2026-08-22 00:19:39 -0400

    ci: revert unsupported BusyBox hash wrapper

--- tests/busybox-ash-nohash.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Run one regression script under BusyBox ash with command hashing disabled so
-# PATH-prepended command stubs replace commands resolved earlier in the script.
-
-[ "$#" -gt 0 ] || exit 2
-script="$1"
-shift
-exec /usr/bin/busybox ash -c 'set +h; . "$0"' "${script}" "$@"
